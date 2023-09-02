7b422a3f199fd761667d1f4abe0de056ee7d1ffd
Md Minhazur Rahman <rafi.rahman03@yahoo.com>
2023-09-02 17:14:10 +0600

    migration file added.

---
+++ prisma/migrations/20230901131435_remove_order_book/migration.sql
@@ -0,0 +1,18 @@
+/*
+  Warnings:
+
+  - You are about to drop the `OrderedBook` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `orderedBooks` to the `Order` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "OrderedBook" DROP CONSTRAINT "OrderedBook_bookId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "OrderedBook" DROP CONSTRAINT "OrderedBook_orderId_fkey";
+
+-- AlterTable
+ALTER TABLE "Order" ADD COLUMN     "orderedBooks" JSONB NOT NULL;
+
+-- DropTable
+DROP TABLE "OrderedBook";
